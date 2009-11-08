b265a2e36f9faa1d71555543507f44f8cf5b8122
Bastian Blank <waldi@debian.org>
2009-11-08 11:10:34 +0100

    Convert Media into file object

--- lib/dvdvideo/udf/media.py
+++ lib/dvdvideo/udf/media.py
@@ -4,13 +4,19 @@ from .volume import Volume
 
 class Media(object):
     def __init__(self, filename):
-        self._media = open(filename, 'rb')
+        self._file = open(filename, 'rb')
+        self._sector_size = 2048
 
-    def read_sector(self, sector, count=2048):
-        count_align = (count + 2047) & ~2047
-        # TODO
-        self._media.seek(sector * 2048)
-        return self._media.read(count_align)
+    def read(self, count):
+        count_align = (count + self._sector_size - 1) & ~(self._sector_size - 1)
+        return self._file.read(count_align)
+
+    def read_sector(self, offset, count=2048):
+        self.seek(offset)
+        return self.read(count)
+
+    def seek(self, offset):
+        return self._file.seek(offset * self._sector_size)
 
     @property
     def volume(self):
