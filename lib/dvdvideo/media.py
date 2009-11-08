200ef38ffa22de4e0586acd68873d13324c35aa4
Bastian Blank <waldi@debian.org>
2009-11-08 11:40:10 +0100

    Add sector file functions

--- lib/dvdvideo/media.py
+++ lib/dvdvideo/media.py
@@ -6,11 +6,33 @@ class Media(object):
 
 
 class MediaUdf(Media):
+    class File(object):
+        def __init__(self, filename):
+            f = open(filename, 'rb')
+            self.close = f.close
+            self.read = f.read
+            self.seek = f.seek
+            self.tell = f.tell
+
+        def read_sector(self, count):
+            return self.read(count * 2048)
+
+        def seek_sector(self, offset):
+            self.seek(offset * 2048)
+
+        def tell_sector(self):
+            return self.tell() // 2048
+
     def __init__(self, filename):
         from .udf.media import Media
 
-        self._file = open(filename, 'rb')
-        self.udf = Media(self._file)
+        f = self.File(filename)
+        self.read = f.read_sector
+        self.seek = f.seek_sector
+        self.tell = f.tell_sector
+
+        self._file = f
+        self.udf = Media(f)
         self.video_dir = self.udf.volume.partitions[0].fileset.root.tree['VIDEO_TS'].entry.tree
 
     def file(self, name):
@@ -20,15 +42,6 @@ class MediaUdf(Media):
             raise NotImplementedError
 
         return f
-
-    def read(self, count):
-        return self._file.read(count * 2048)
-
-    def seek(self, offset):
-        self._file.seek(offset * 2048)
-
-    def tell(self):
-        return self._file.tell() // 2048
 
     def vmg(self):
         return VmgUdf(self)
