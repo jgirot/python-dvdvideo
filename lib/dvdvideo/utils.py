e0e9153ad5c0fa73816f488f3763045e9ce28d8d
Bastian Blank <waldi@debian.org>
2009-12-04 22:39:09 +0100

    Split progress into stream and meter

--- lib/dvdvideo/utils.py
+++ lib/dvdvideo/utils.py
@@ -1,51 +1,63 @@
-class ProgressStream(object):
-    def __init__(self, stream, total=-1, count=0):
+class ProgressMeter(object):
+    def __init__(self, stream, total, count=0):
         self.stream = stream
         self.total = total
         self.count = count
 
         self.meter_ticks = 60
 
-        self._display()
+        stream.set_meter(self)
 
-    def _clear(self):
-        self.stream.write(chr(27) + '[M')
-        self.stream.write(chr(27) + '[G')
+    def clear(self):
+        self.stream.write_real(chr(27) + '[M' + chr(27) + '[G')
 
-    def _display(self):
+    def display(self):
         value = int(self.count / self.total * self.meter_ticks)
 
         meter = ['[%s>%s]' % ('-' * value, ' ' * (self.meter_ticks - value))]
+        meter.append('%d/%d %d%%' % (self.count, self.total, self.count / self.total * 100))
 
-        if self.total > 0:
-            perc = self.count / self.total * 100
-            meter.append('%d/%d %d%%' % (self.count, self.total, perc))
-        else:
-            meter.append('%d' % self.count)
-
-        self.stream.write(' '.join(meter))
+        self.stream.write_real(' '.join(meter))
         self.stream.flush()
-
-    def close(self):
-        self._clear()
 
     def set(self, count):
         self.count = min(count, self.total)
-        self._clear()
-        self._display()
-
-    def set_total(self, total):
-        self.total = total
-        self._clear()
-        self._display()
+        self.clear()
+        self.display()
 
     def update(self, count):
         self.set(self.count + count)
 
     def write(self, str):
-        self._clear()
-        self.stream.write(str)
-        if str[-1] != '\n':
-            self.stream.write('\n')
-        self._display()
+        self.clear()
+        self.stream.write_real(str)
+        self.display()
 
+
+class ProgressStream(object):
+    def __init__(self, stream):
+        self.stream = stream
+
+        self._meter = None
+
+    def clear_meter(self):
+        self.set_meter(None)
+
+    def flush(self):
+        self.stream.flush()
+
+    def set_meter(self, meter):
+        if self._meter:
+            self._meter.clear()
+        self._meter = meter
+        if self._meter:
+            self._meter.display()
+
+    def write(self, str):
+        if self._meter:
+            self._meter.write(str)
+        else:
+            self.stream.write(str)
+
+    def write_real(self, str):
+        return self.stream.write(str)
