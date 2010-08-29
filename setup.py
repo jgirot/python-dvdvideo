6434e96ee981297c63c4bb1c59d406001fa5b69d
Bastian Blank <waldi@debian.org>
2010-08-29 20:25:29 +0200

    Add setup script

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+from distutils.core import setup
+
+setup(
+        name='python-dvdvideo',
+        version='0.1',
+        author='Bastian Blank',
+        author_email='waldi@debian.org',
+        packages=('dvdvideo', 'dvdvideo.udf'),
+        package_dir = {'': 'lib'},
+        scripts=('bin/dvd-video-backup-image', ),
+)
