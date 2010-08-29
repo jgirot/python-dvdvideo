02e1d71d7ef2bfa7b6386df580d6f38075fdce70
Bastian Blank <waldi@debian.org>
2010-08-29 20:12:58 +0200

    Add license information

--- lib/dvdvideo/libdvdcss.py
+++ lib/dvdvideo/libdvdcss.py
@@ -1,3 +1,19 @@
+"""
+@copyright: 2009 Bastian Blank <waldi@debian.org>
+@license: GNU GPL-3
+"""
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 3 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 from ctypes import CDLL, CFUNCTYPE, c_char_p, c_int, c_void_p, create_string_buffer
 
 try:
