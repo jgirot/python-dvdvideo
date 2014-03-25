626cfe97b41ab4b8096d30313c30489e9eba10d0
Bastian Blank <waldi@debian.org>
2014-03-25 22:26:36 +0100

    Add tests for dvdvideo.udf.general.*AD

--- dvdvideo/udf/test_general.py
+++ dvdvideo/udf/test_general.py
@@ -38,3 +38,58 @@ class TestOSTACompressedUnicode:
         with pytest.raises(ValueError):
             OSTACompressedUnicode(i)
 
+
+class TestExtentAD:
+    def test_size(self):
+        assert ExtentAD.size == 20
+
+    def test_decode_0(self):
+        e = ExtentAD(b'\x00'*ExtentAD.size)
+        assert e.flags == 0
+        assert e.length == 0
+        assert e.location == 0
+        assert e.partition == 0
+
+    def test_decode_1(self):
+        e = ExtentAD(b'\xff'*ExtentAD.size)
+        assert e.flags == 0x3
+        assert e.length == 0x3fffffff
+        assert e.location == 0xffffffff
+        assert e.partition == 0xffff
+
+
+class TestLongAD:
+    def test_size(self):
+        assert LongAD.size == 16
+
+    def test_decode_0(self):
+        e = LongAD(b'\x00'*LongAD.size)
+        assert e.flags == 0
+        assert e.length == 0
+        assert e.location == 0
+        assert e.partition == 0
+
+    def test_decode_1(self):
+        e = LongAD(b'\xff'*LongAD.size)
+        assert e.flags == 0x3
+        assert e.length == 0x3fffffff
+        assert e.location == 0xffffffff
+        assert e.partition == 0xffff
+
+
+class TestShortAD:
+    def test_size(self):
+        assert ShortAD.size == 8
+
+    def test_decode_0(self):
+        e = ShortAD(b'\x00'*ShortAD.size)
+        assert e.flags == 0
+        assert e.length == 0
+        assert e.location == 0
+
+    def test_decode_1(self):
+        e = ShortAD(b'\xff'*ShortAD.size)
+        assert e.flags == 0x3
+        assert e.length == 0x3fffffff
+        assert e.location == 0xffffffff
+
